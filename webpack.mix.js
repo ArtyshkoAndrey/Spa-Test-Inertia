09e6ca4157b32f3ae186e0aacc46f4f6195e20c9
Fulliton <artyshko.andrey@gmail.com>
2021-12-25 18:40:40 +0700

    test

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,4 +15,4 @@ const {browserSync} = require("laravel-mix");
 mix.js('resources/js/app.js', 'public/js').vue()
   .sass('resources/sass/app.scss', 'public/css')
   .disableNotifications()
-  .browserSync('http://spa')
+  .browserSync('https://spa')
